91a2f923d977c2f87de4f4583b65d1d259447e10
Corey <corey6616@hotmail.com>
2024-03-24 21:08:52 -0700

    Pizzeria App

--- app/src/main/java/com/example/norththompsonpizzarialandingactivity/CustomerReviews.java
+++ app/src/main/java/com/example/norththompsonpizzarialandingactivity/CustomerReviews.java
@@ -8,13 +8,20 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class CustomerReviews extends AppCompatActivity {
 
     ImageButton logoBtn;
     RatingBar ratingBar;
 
-    TextView review1,review2,review3,review4,review5;
+    TextView review2, review3, review4, review5;
+
+    List<String> displayedReviews = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +30,18 @@ public class CustomerReviews extends AppCompatActivity {
 
         logoBtn = findViewById(R.id.logoBtn);
         ratingBar = findViewById(R.id.averageStars);
-        review1 = findViewById(R.id.customerReviewTv1);
         review2 = findViewById(R.id.customerReviewTv2);
         review3 = findViewById(R.id.customerReviewTv3);
         review4 = findViewById(R.id.customerReviewTv4);
         review5 = findViewById(R.id.customerReviewTv5);
+
+        String[] reviews = getResources().getStringArray(R.array.reviews);
+
+        displayReviews(review2, reviews);
+        displayReviews(review3, reviews);
+        displayReviews(review4, reviews);
+        displayReviews(review5, reviews);
+
 
         logoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +51,32 @@ public class CustomerReviews extends AppCompatActivity {
 
             }
         });
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(CustomerReviews.this, String.valueOf(rating), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
-}
+
+        private void displayReviews (TextView textView, String[]reviews){
+            String randomReview = getRandomReview(reviews);
+            textView.setText(randomReview);
+            displayedReviews.add(randomReview);
+        }
+
+        private String getRandomReview (String[]reviews){
+
+            List<String> allReviews = new ArrayList<>();
+            for (String review : reviews) {
+                if (!displayedReviews.contains(review)) {
+                    allReviews.add(review);
+                }
+            }
+            Random random = new Random();
+            int index = random.nextInt(allReviews.size());
+            return allReviews.get(index);
+        }
+    }
