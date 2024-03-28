3baf755c5456b8fc9ed387ddf4c4774e70b4857a
ethanbukkos <ethan.bukkos@gmail.com>
2024-03-28 15:07:23 -0700

    UI updates from Ethan

--- app/src/main/java/com/example/norththompsonpizzarialandingactivity/ConfirmationActivity.java
+++ app/src/main/java/com/example/norththompsonpizzarialandingactivity/ConfirmationActivity.java
@@ -3,12 +3,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
